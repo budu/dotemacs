e75dd027c0dae524056129627b72d8e1e62580fa
Nicolas Buduroi <nbuduroi@gmail.com>
2025-11-18 14:38:55 -0500

    Add simpler keybinding for flycheck-next-error

--- mu/flycheck.el
+++ mu/flycheck.el
@@ -11,6 +11,8 @@
 (use-package flycheck
   :diminish flycheck-mode
   :hook (prog-mode . flycheck-mode)
+  :bind (:map flycheck-mode-map
+              ("C-c n" . flycheck-next-error))
   :custom ((flycheck-check-syntax-automatically '(save mode-enabled))
            (flycheck-display-errors-delay 0.25)
            (flycheck-emacs-lisp-load-path 'inherit)))
