ef1889745965fc289d23a3b2906372f758cf5e04
Nicolas Buduroi <nbuduroi@gmail.com>
2026-02-04 11:21:58 -0500

    Make ispell ignore commit hashes

--- mu/spelling.el
+++ mu/spelling.el
@@ -8,11 +8,16 @@
 ;;
 ;;; Code:
 
+(defun mu/flyspell-skip-commit-hashes (beg end _poss)
+  "Skip flyspell highlighting for git commit hashes (7-40 hex chars)."
+  (string-match-p "^[0-9a-f]\\{7,40\\}$" (buffer-substring-no-properties beg end)))
+
 (use-package flyspell
   :hook ((text-mode . flyspell-mode)
          (agent-shell-mode . flyspell-mode)
          (prog-mode . flyspell-prog-mode))
   :config
+  (add-hook 'flyspell-incorrect-hook #'mu/flyspell-skip-commit-hashes)
   (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
     (add-hook hook (lambda () (flyspell-mode -1)))))
 
