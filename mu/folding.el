f609c09de509bb5dc7fe6a729f8733ce319731fa
Nicolas Buduroi <nbuduroi@gmail.com>
2026-08-28 11:05:20 -0400

    Fix loading warnings

--- mu/folding.el
+++ mu/folding.el
@@ -8,6 +8,14 @@
 ;;
 ;;; Code:
 
+;; Origami computes this face from `highlight' while loading.  On Emacs 31's
+;; initial frame that background can still be `unspecified', which is not a
+;; valid box color and prevents packages that load Origami indirectly (such as
+;; dap-mode via lsp-mode) from initializing.
+(when (version<= "31" emacs-version)
+  (custom-set-faces
+   '(origami-fold-header-face ((t (:inherit highlight :box nil))))))
+
 ;; old but working, might want to look into ts-fold
 (use-package origami
   :bind (("C-c v" . origami-recursively-toggle-node)
